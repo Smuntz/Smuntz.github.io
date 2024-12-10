900e667a5bfb8f3d5a6d53dccda0d6efba0e1ac9
Smuntz <eldude73@gmail.com>
2024-12-10 22:05:25 +1100

    REPLACING HOMEPAGE
    moved home page to its own repository called
    claculators and replacing it with my resume.

--- rent.js
+++
@@ -1,7 +0,0 @@
-const rent = document.getElementById("rent");
-let p = document.querySelector("p");
-p.innerHTML = "Monthly Rent $";
-
-function interest() {
-    p.innerHTML = "Monthly Rent: $" + ((Number(rent.value) * 52) / 12).toFixed(2);
-}
